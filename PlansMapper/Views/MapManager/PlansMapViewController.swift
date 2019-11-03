b26b11f6db972497c7c008caa5dec109749cb636
maurice_jm <julesmaurice.mulisa@gmail.com>
2019-11-03 17:13:44 +0400

    can locate and pin different items on the map

--- PlansMapper/Views/MapManager/PlansMapViewController.swift
+++ PlansMapper/Views/MapManager/PlansMapViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class PlansMapViewController: UIViewController {
+class PlansMapViewController: UIViewController ,MKMapViewDelegate {
 
 	@IBOutlet var mapView: MKMapView!
-	let regionRadius: CLLocationDistance = 1000
+	let regionRadius: CLLocationDistance = 3000
 	lazy var viewModel = PlansMapViewModel()
 	var planVenues: [PlansMapModel] = []
 	var planVenue: PlansMapModel?
 	var currentUserLocation : CLLocation!
-	var fakeLocation : CLLocation!
+	var fakeLocation = CLLocation(latitude: -20.161971, longitude:  57.503238)
 	var locationAllowed = false
 	let locationManager = CLLocationManager()
 	
@@ -31,7 +31,7 @@ class PlansMapViewController: UIViewController {
 		// set initial location as the current user location
 		checkLocationAuthorizationStatus()
 		
-		centerMapOnLocation(location: currentUserLocation)
+		centerMapOnLocation(location: fakeLocation)  // use the current location var to get dynamic location
 		
 		mapView.register(ArtworkMarkerView.self,
 						 forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
@@ -39,7 +39,7 @@ class PlansMapViewController: UIViewController {
 		//mapView.setRegion(planVenue.myRegion, animated: true)
 		//loadInitialData()
 		
-		searchPlaces(for: "Shopping Mall")
+		searchPlaces(for: "shopping mall")
     }
 	
 	func centerMapOnLocation(location: CLLocation) {
@@ -48,23 +48,21 @@ class PlansMapViewController: UIViewController {
 		mapView.setRegion(coordinateRegion, animated: true)
 	}
 	
-	func searchPlaces(for item:String) {
+	func searchPlaces(for itemName:String) {
 		let searchRequest = MKLocalSearch.Request()
-		searchRequest.naturalLanguageQuery = "Shopping Mall"
-		
+		searchRequest.naturalLanguageQuery = itemName
+		let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
+		searchRequest.region = MKCoordinateRegion(center: currentUserLocation.coordinate, span: span)
 		let search = MKLocalSearch(request: searchRequest)
 		
 		search.start(completionHandler: { (response, error) in
-			
+		
+			print("Found: \(response!.mapItems.count) items on the map")
 			for item in response!.mapItems {
-				let venue = PlansMapModel(locationName: item.name!, coordinate: CLLocationCoordinate2D(latitude: item.placemark.coordinate.latitude, longitude: item.placemark.coordinate.longitude))
-				
-				self.planVenues.append(venue)
-				print("Found: \(venue.locationName), at lat: \(venue.coordinate.latitude), and at lon: \(venue.coordinate.longitude)")
-				
+				self.addPinToMapView(title: item.name, latitude: item.placemark.location!.coordinate.latitude, longitude: item.placemark.location!.coordinate.longitude)
+				print("Found item at: \(item.name!)")
 			}
 			
-			self.mapView.addAnnotations(self.planVenues)
 
 //			guard let response = response else {
 //				print("Error: \(error?.localizedDescription ?? "Unknown error").")
@@ -76,6 +74,20 @@ class PlansMapViewController: UIViewController {
 		
 	})
 	}
+	
+	
+	func addPinToMapView(title: String?, latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+		if let title = title {
+			let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+			let annotation = MKPointAnnotation()
+			
+			annotation.coordinate = location
+			annotation.title = title
+			
+			mapView.addAnnotation(annotation)
+		}
+	}
+	
 	
 	func loadInitialData() {
 		guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json") else { return }
@@ -100,10 +112,10 @@ class PlansMapViewController: UIViewController {
 		}
 	}
 	
-}
-
-
-extension PlansMapViewController : MKMapViewDelegate {
+	
+	
+	
+	
 	
 	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
 		guard let annotation = annotation as? PlansMapModel else { return nil }
@@ -129,5 +141,12 @@ extension PlansMapViewController : MKMapViewDelegate {
 		location.mapItem().openInMaps(launchOptions: launchOptions)
 	}
 	
-
+	
 }
+
+
+//extension PlansMapViewController : MKMapViewDelegate {
+//
+//
+//
+//}
