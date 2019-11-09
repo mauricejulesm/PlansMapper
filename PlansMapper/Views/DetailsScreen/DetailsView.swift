5361ed1824b1b39997887fabba48a02e92a9ab9c
maurice_jm <julesmaurice.mulisa@gmail.com>
2019-11-09 22:43:55 +0400

    added a dummy details view

--- PlansMapper/Views/DetailsScreen/DetailsView.swift
+++ PlansMapper/Views/DetailsScreen/DetailsView.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class DetailsView: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet var planTitleLbl: UILabel!
+	@IBOutlet var planDescLbl: UILabel!
+	@IBOutlet var catPredictLbl: UILabel!
+	@IBOutlet var languageLbl: UILabel!
+	@IBOutlet var placeNamesLbl: UILabel!
+	@IBOutlet var peopleNamesLbl: UILabel!
+	@IBOutlet var recommendedVenueLbl: UILabel!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	@IBAction func viewVenuesBtn(_ sender: Any) {
+		
+	}
 
 }
